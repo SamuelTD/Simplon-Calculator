132395143affe8cf732c76d7100b811c04936dc5
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2024-10-23 11:31:53 +0200

    create get_input function in main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from division import division
 
+x = 0
+y = 0
+
+def get_input() -> None:
+    """
+    Ask user for two numbers. Keep asking until two valide numbers are inputed.    
+    """
+    #Keep asking the user for numbers until valid numbers are entered.
+    while True:
+        try:
+            x = float(input("Veuillez entrer le premier nombre :"))
+            y = float(input("Veuillez entrer le deuxième nombre :"))
+            break
+        except:
+            print("Veuillez n'entrer que des chiffres.")
 
 
 
